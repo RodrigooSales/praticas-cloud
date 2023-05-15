9a554687d544a4b5de4d836476de5e36bff5bd0f
Rodrigo Sales <t.rodricksales10@gmail.com>
2023-05-15 20:21:42 -0300

    subindo inicio do projeto final

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxleG9rZnBmamp1bnZncW1xZ2hwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQxOTA2MTksImV4cCI6MTk5OTc2NjYxOX0.ut07UzBcD_-bRmysh7v37Fwa0qDHYHdzogccGYiMNuU
+// https://lexokfpfjjunvgqmqghp.supabase.co
+
+const { createClient } = require('@supabase/supabase-js');
+
+const supabase = createClient(
+    'https://lexokfpfjjunvgqmqghp.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxleG9rZnBmamp1bnZncW1xZ2hwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQxOTA2MTksImV4cCI6MTk5OTc2NjYxOX0.ut07UzBcD_-bRmysh7v37Fwa0qDHYHdzogccGYiMNuU'
+  )
+
 const express = require("express"); //importa o módulo express neste arquivo
 const app = express(); //iniciando o express
 
-app.get('/', (req, res) => {
-    res.send('<h1>Bem-vindo à minha página inicial!</h1>');
+app.get('/produtos', async (req, res) => {
+    let { data: produtos, error } = await supabase
+    .from('produtos')
+    .select('*')
 });
-  
-app.get('/sobre', (req, res) => {
-    res.send('<h1>Sobre mim</h1><p>Eu sou um estudante de analise e desenvolvimento de sistemas.</p>');
-});
-  
-app.get('/contato', (req, res) => {
-    res.send('<h1>Entre em contato comigo</h1><p>Email: teixeirarodrigo2311@gmail.com<br>Github: <a href="https://github.com/RodrigooSales/">https://github.com/RodrigooSales</a></p>');
-});
-
-app.get("/contato/email", function(req,res){
-    res.send("<h1>Meu Email para contato</h1> <p>Email: teixeirarodrigo2311@gmail.com</p>");
-})
-
-app.get("/contato/github", function(req,res){
-    res.send('<h1>Meu portifolio no Github!</h1> <p>Github: <a href="https://github.com/RodrigooSales/">https://github.com/RodrigooSales</a></p>');
-})
   
 app.listen(process.env.PORT ?? 3000,function(erro){
     if (erro){
